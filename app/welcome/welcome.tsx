96bf288cad790d6302e8838acdf67a587404a1ee
Siphamandla Mbatha <siphamandlambatha@Siphamandlas-MacBook-Pro.local>
2026-08-06 19:46:00 +0200

    added creating newElement

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,65 +1,101 @@
 import { useRef, useState } from "react"
 
 type Position = {
-  x?:number
-  y?:number
+  x?: number
+  y?: number
 }
 
-export function Welcome(){
+export function Welcome() {
 
-  const [boxPosition, setBoxPosition] = useState<Record<string, Position>>( { "box_1":{x:0, y:0} });
+  const [boxPosition, setBoxPosition] = useState<Record<string, Position>>({ "box_1": { x: 0, y: 0 } });
   const selectedTarget = useRef<string | null>(null);
-  const pointerOffset = useRef<Position>({x:0, y:0 });
+  const pointerOffset = useRef<Position>({ x: 0, y: 0 });
 
-  const HandleMove = (event: React.PointerEvent)=>{
+  const HandleMove = (event: React.PointerEvent) => {
 
-    if(!selectedTarget.current)return;
-
+    if (!selectedTarget.current) return;
 
     const rect = event.currentTarget.getBoundingClientRect();
 
     const x = (event.clientX - rect.left) - (pointerOffset.current.x ?? 0);
-    const y = (event.clientY - rect.top) - (pointerOffset.current.y ?? 0) ;
-
-    const box = selectedTarget.current 
-   setBoxPosition(prev => ({...prev,[box]: { x, y } }));
+    const y = (event.clientY - rect.top) - (pointerOffset.current.y ?? 0);
+    
+    const box = selectedTarget.current
+    console.log(boxPosition[box]);
+    setBoxPosition(prev => ({ ...prev, [box]: { x, y } }));
   }
 
-  const HandlePointerDown = (event: React.PointerEvent)=>{
+  const createNewBox = (event: React.PointerEvent) => {
+    const boxID = crypto.randomUUID();
 
-    if(event.target instanceof HTMLElement){
+    const topPosition =  event.clientY;
+    const leftPosition =  event.clientX;
 
-      if(event.target.getAttribute("id") === "container")return;
+    const newElement = Element({ top:topPosition, left:leftPosition, id:boxID });
+    setBoxPosition(prev => ({ ...prev, [boxID]: { x:leftPosition, y: topPosition } }));
+
+  }
+
+  const HandlePointerDown = (event: React.PointerEvent) => {
+
+
+    if (event.target instanceof HTMLElement) {
+
+      if (event.target.getAttribute("id") === "container") {
+        createNewBox(event);
+        return;
+      }
 
       const rect = event.target.getBoundingClientRect();
 
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
 
-      selectedTarget.current = event.target.getAttribute("id"); 
-      pointerOffset.current = {x, y};
+      selectedTarget.current = event.target.getAttribute("id");
+      pointerOffset.current = { x, y };
     }
   }
 
-  const HandlePointerUp = (event: React.PointerEvent)=>{
+  const HandlePointerUp = (event: React.PointerEvent) => {
     selectedTarget.current = null;
-    pointerOffset.current = { x:0, y:0};
+    pointerOffset.current = { x: 0, y: 0 };
   }
 
-  return(
-    <div 
-    id="container"
-    onPointerDown={HandlePointerDown} 
-    onPointerMove={HandleMove}
-    onPointerUp={HandlePointerUp}  className="bg-yellow-400 w-full h-full relative " >
+
+  return (
+    <div
+      id="container"
+      onPointerDown={HandlePointerDown}
+      onPointerMove={HandleMove}
+      onPointerUp={HandlePointerUp} className="bg-yellow-400 w-full h-full relative " >
 
       <div
         id="box_1"
-        style={{ top: boxPosition["box_1"].y , left:boxPosition["box_1"].x }}  
+        style={{ top: boxPosition["box_1"].y, left: boxPosition["box_1"].x }}
         className="bg-amber-600 absolute w-40 aspect-square right-32 " >
-        
       </div>
+      {
+        Object.entries(boxPosition).map(([ id, position] ) => <Element key={id} id={id} left={position.x!} top={position.y!} /> )
+      }
 
     </div>
   )
 }
+
+type ElementProps = {
+  top:number
+  left:number
+  id:string
+};
+
+export function Element( props:ElementProps ){
+  return(
+    <div
+      key={props.id}
+        id={props.id}
+        style={{ top: props.top, left:props.left }}
+        className="bg-amber-600 absolute w-40 aspect-square right-32 " >
+      </div>
+
+  );
+};
